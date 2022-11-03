877ca29ae7c509f0ceac9f43225f972e4c58d12f
d4nQw3rty <danlinmoy@gmail.com>
2022-11-03 18:09:29 -0400

    Create navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',      
+    },
+    {
+      id: 2,
+      path: '/Categories',
+      text: 'Categories',
+    },
+  ];
+
+const Navbar = () => {
+    <>
+      <header>
+        <h1>Bookstore CMS</h1>
+        <nav>
+            <ul>
+                {links.map(link => (
+                    <li key={link.id}>
+                        <NavLink to={link.path} exact>{link.text}</NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    </header>
+    </>
+}
+
+export default Navbar;
+  
