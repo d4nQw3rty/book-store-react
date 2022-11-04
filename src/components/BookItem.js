96e1fc1a48dc6c54b83c84ccb5e76e681764eebf
d4nQw3rty <danlinmoy@gmail.com>
2022-11-04 09:39:52 -0400

    book item recieve props

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const BookIem = () => (
+const BookIem = (props) => (
+    
   <div>
     <h2>BooksList</h2>
     <div>
       <div>
-        <h3>Book 1</h3>
-        <p>Author 1</p>
+        <h3>{props.title}</h3>
+        <p>{props.author}</p>
       </div>
       <button type="button">Remove</button>
     </div>
