16772d2a8d3a5d89478f6a352e92bca05da30a42
d4nQw3rty <danlinmoy@gmail.com>
2022-11-08 11:00:55 -0400

    Built action creator
    Write action creator:  addBook, and removeBook
    export the action creator.

--- src/redux/books/Book.js
+++ src/redux/books/Book.js
@@ -1,3 +1,16 @@
 // Actions
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
+
+// action creators
+const addBook = (book) => ({
+  type: ADD_BOOK,
+  payload: book,
+});
+
+const removeBook = (id) => ({
+  type: REMOVE_BOOK,
+  payload: id,
+});
+
+export { addBook, removeBook };
