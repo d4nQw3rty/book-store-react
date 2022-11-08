e53228271aa92d1fed352ef7be6fc26f2dfafee6
d4nQw3rty <danlinmoy@gmail.com>
2022-11-08 18:38:19 -0400

    Import useselector and check if display books list

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import BookItem from './BookItem';
+import { useSelector } from 'react-redux';
 
-const BookList = () => (
-  <ul>
-    <BookItem />
-  </ul>
-);
+const BookList = () => {
+  const books = useSelector((state) => state.books);
+
+  return (
+    <ul>
+      {books.map((book) => (
+        <li key={book.id}>
+          <div>
+            <div>
+              <h3>{book.title}</h3>
+              <p>{book.author}</p>
+            </div>
+            <button type="button">Remove</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default BookList;
