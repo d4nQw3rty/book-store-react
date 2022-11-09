0b0b67547e09b3b8db803045e76804124b11b962
d4nQw3rty <danlinmoy@gmail.com>
2022-11-09 18:33:10 -0400

    Add middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -7,6 +7,7 @@ const store = configureStore({
     books: booksReducer,
     categories: categoriesReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 
 export default store;
