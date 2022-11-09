6af40abf6dcc6cc1acddf97a7af4411dc4b83399
d4nQw3rty <danlinmoy@gmail.com>
2022-11-09 12:37:39 -0400

    Add a functionality for "Remove" button click

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/Book';
 
 const BookIem = () => {
+  const dispatch = useDispatch();
+  const handleRemoveBook = (id) => {
+    dispatch(removeBook(id));
+  };
   const books = useSelector((state) => state.books);
   return (
     books.map((book) => (
       <li key={book.id}>
         <h3>{book.title}</h3>
         <p>{book.author}</p>
+        <button type="button" onClick={() => handleRemoveBook(book.id)}>Remove Book</button>
       </li>
     ))
   );
