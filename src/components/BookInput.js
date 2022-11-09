93cca73755b3e03c51868f60f4742fbd8ef8042b
d4nQw3rty <danlinmoy@gmail.com>
2022-11-09 11:49:50 -0400

    import dispatch, addBook and v4 in inputBook

--- src/components/BookInput.js
+++ src/components/BookInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import { useDispatch } from 'react-redux';
-// import { v4 as uuidv4 } from 'uuid';
-// import { addBook } from '../redux/books/Book';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/Book';
 
 const BookInput = () => (
   <div>
