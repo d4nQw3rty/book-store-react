16194a8cf8ab643f5a1c7d72aa4be7660a650573
d4nQw3rty <danlinmoy@gmail.com>
2022-11-09 18:53:02 -0400

    Update store to use thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,17 @@
+import { combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/Book';
 import categoriesReducer from './categories/categories';
 
-const store = configureStore({
-  reducer: {
-    books: booksReducer,
-    categories: categoriesReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+const rootReducer = combineReducers({
+  books: booksReducer,
+  categories: categoriesReducer,
 });
+
+const store = configureStore({
+  reducer: rootReducer,
+},
+applyMiddleware(thunk));
 
 export default store;
