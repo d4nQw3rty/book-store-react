e51c8a146787780471fd98b9aa70c08279543e66
d4nQw3rty <danlinmoy@gmail.com>
2022-11-09 12:19:54 -0400

    Use dispatch and useState to send new book to list
    -Create handleSubmit  for avoiding empty fields submited in the form and save book information
    -create  newBook variable to add ramdom id to new book submited in the input field by uuidv4.
    -use dispatch to call addBook and pass newbook object info to book reducer that will send new new Object with new info to the store to display in the list book.

--- src/components/BookInput.js
+++ src/components/BookInput.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/Book';
 
-const BookInput = () => (
-  <div>
-    <h1>ADD NEW BOOK</h1>
-    <form action="">
-      <input
-        type="text"
-        name="bookTitle"
-        id="book-title"
-        placeholder="Title"
-        required
-      />
-      <input
-        type="text"
-        name="bookCategory"
-        id="book-author"
-        placeholder="Author"
-        required
-      />
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </div>
-);
+const BookInput = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default BookInput;
