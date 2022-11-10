e4b00228a821fbd27ddb7f4bb3869b61b33187a6
d4nQw3rty <danlinmoy@gmail.com>
2022-11-10 16:56:24 -0400

    refactoring bookstore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,17 +1,14 @@
-import { combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/Book';
 import categoriesReducer from './categories/categories';
 
-const rootReducer = combineReducers({
+const reducer = combineReducers({
   books: booksReducer,
   categories: categoriesReducer,
 });
 
 const store = configureStore({
-  reducer: rootReducer,
-},
-applyMiddleware(thunk));
+  reducer,
+});
 
 export default store;
