8d5e6ba79eefe0fa8374d3e8da68d7688844a37b
d4nQw3rty <danlinmoy@gmail.com>
2022-11-11 15:54:08 -0400

    Set category selection to fit the disign

--- src/components/BookInput.js
+++ src/components/BookInput.js
@@ -35,6 +35,16 @@ const BookInput = () => {
       <form onSubmit={submitBookToStore}>
         <input type="text" placeholder="Title" value={title} onChange={handleTitleChange} required />
         <input type="text" placeholder="Author" value={author} onChange={handleAuthorChange} required />
+        <select name="category" id="category" value={category} onChange={(e) => setCategory(e.target.value)} required>
+          <option value="">Select Category</option>
+          <option value="Action">Action</option>
+          <option value="Biography">Biography</option>
+          <option value="History">History</option>
+          <option value="Horror">Horror</option>
+          <option value="Kids">Kids</option>
+          <option value="Learning">Learning</option>
+          <option value="Sci-Fi">Sci-Fi</option>
+        </select>
         <button type="submit">Add Book</button>
       </form>
     </div>
