e67f4354bcaaefd60e30574e1e1b95d8ffa4a43a
d4nQw3rty <danlinmoy@gmail.com>
2022-11-11 09:56:39 -0400

    Refactor BookItem to display books from API
    Display and remove books from API function

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,23 +1,25 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook, getBooks } from '../redux/books/Book';
+import { fetchBooksAsync, removeBookAsync } from '../redux/books/Book';
 
 const BookIem = () => {
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getBooks());
+    dispatch(fetchBooksAsync());
   }, [dispatch]);
+  const handleRemoveBook = (book) => {
+    dispatch(removeBookAsync(book.id));
+  };
+
   return (
-    <>
-      {books.map((book) => (
-        <li key={book.id}>
-          <h2>{book.title}</h2>
-          <p>{book.author}</p>
-          <button type="button" onClick={() => dispatch(removeBook(book.id))}>Remove Book</button>
-        </li>
-      ))}
-    </>
+    books.map((book) => (
+      <li key={book.id}>
+        <p>{book.title}</p>
+        <p>{book.author}</p>
+        <button type="button" onClick={() => handleRemoveBook(book)}>Remove Book</button>
+      </li>
+    ))
   );
 };
 
