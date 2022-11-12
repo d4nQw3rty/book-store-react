23b3b055bc5f37c6136148e614e6c62d71fba12d
d4nQw3rty <danlinmoy@gmail.com>
2022-11-12 09:12:43 -0400

    add styling in form

--- src/components/BookInput.js
+++ src/components/BookInput.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBookAsync } from '../redux/books/Book';
+import '../App.css';
 
 const BookInput = () => {
   const [title, setTitle] = useState('');
@@ -31,21 +32,12 @@ const BookInput = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={submitBookToStore}>
-        <input type="text" placeholder="Title" value={title} onChange={handleTitleChange} required />
-        <input type="text" placeholder="Author" value={author} onChange={handleAuthorChange} required />
-        <select name="category" id="category" value={category} onChange={(e) => setCategory(e.target.value)} required>
-          <option value="">Select Category</option>
-          <option value="Action">Action</option>
-          <option value="Biography">Biography</option>
-          <option value="History">History</option>
-          <option value="Horror">Horror</option>
-          <option value="Kids">Kids</option>
-          <option value="Learning">Learning</option>
-          <option value="Sci-Fi">Sci-Fi</option>
-        </select>
-        <button type="submit">Add Book</button>
+    <div className="Form-Container">
+      <h2 className="Form-Title">ADD NEW BOOK</h2>
+      <form onSubmit={submitBookToStore} className="Form">
+        <input className="Input-Book input-title" type="text" placeholder="Title" value={title} onChange={handleTitleChange} required />
+        <input className="Input-Book" type="text" placeholder="Author" value={author} onChange={handleAuthorChange} required />
+        <button className="Add-Button" type="submit">ADD BOOK</button>
       </form>
     </div>
   );
